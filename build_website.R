7e00628dddc4bed03687da34e31b4f6733a2298c
Vharuck <4ChanStats@gmail.com>
2017-04-15 22:28:01 -0400

    Created build script

---
+++ build_website.R
@@ -0,0 +1,18 @@
+# Builds the wedding website
+library(rmarkdown)
+library(knitr)
+library(htmltools)
+
+
+opts_chunk$set(message = FALSE)
+
+# Convenience functions
+top_level_image <- function(img_name, img_title) {
+    as.character(img(
+        src = paste0('img/', img_name),
+        title = img_title,
+        width = '640px'
+    ))
+}
+
+render_site()
